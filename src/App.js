94284e45b4fe2a67954d20d09bdb828c88235ba3
junaidbinjaman <me@junaidbinjaman.com>
2024-08-20 10:00:00 +0600

    Started a fresh react project to practice contex api and redux

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-import { Provider } from "react-redux";
-import "./App.css";
-import CakeContainer from "./components/CakeContainer";
-import HooksCakeContainer from "./components/HooksCakeContainer";
-import IceCream from "./components/IceCream";
-import ItemContainer from "./components/ItemContainer";
-import NewCakeContainer from "./components/NewCakeContainer";
-import store from "./redux/store";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Provider store={store}>
-      <CakeContainer />
-      {/* comming from state */}
-      <HooksCakeContainer />
-      {/* ice cream */}
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <p>Icecream is here</p>
-      <IceCream />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <p>Buy Multiple iceCream</p>
-      <NewCakeContainer />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <ItemContainer cake />
-      <ItemContainer  />
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/redux/index.js
+++
@@ -1,3 +0,0 @@
-export { buyCake } from "./cake/cakeActions";
-export { buyIceCream } from "./iceCream/icreCreamAction";
-
