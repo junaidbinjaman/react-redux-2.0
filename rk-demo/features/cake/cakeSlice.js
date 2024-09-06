c948c5908fded0b73531694e120aeb92c3ff964c
junaidbinjaman <me@junaidbinjaman.com>
2024-09-06 19:27:43 +0600

    Started learing redux toolkit

---
+++ rk-demo/features/cake/cakeSlice.js
@@ -0,0 +1,21 @@
+const {createSlice} = require('@reduxjs/toolkit');
+
+const initialState = {
+    numOfCake: 10,
+};
+
+const cakeSlice = createSlice({
+    name: 'cake',
+    initialState,
+    reducers: {
+        ordered: (state) => {
+            state.numOfCake--;
+        },
+        restocked: (state, action) => {
+            state.numOfCake += action.payload;
+        }
+    },
+});
+
+module.exports = cakeSlice.reducer;
+module.exports.cakeActions = cakeSlice.actions;
